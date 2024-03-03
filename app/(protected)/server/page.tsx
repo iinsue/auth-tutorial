7e0a56c71cf556754716d3b085e55ed7fdf8529a
iinsue <hinsu93@gmail.com>
2024-03-03 22:03:02 +0900

    Server Side 에서 유저 정보 불러오기

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,13 @@
+import { currentUser } from "@/lib/auth";
+
+const ServerPage = async () => {
+  const user = await currentUser();
+
+  return (
+    <>
+      <div>{JSON.stringify(user)}</div>
+    </>
+  );
+};
+
+export default ServerPage;
